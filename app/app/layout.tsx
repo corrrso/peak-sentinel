497abfe470df36a592072edfe139522273b10aeb
Luca Corsini <lcorsini@ea.com>
2026-05-11 10:37:40 +0100

    iframeResizer to get page height

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import IframeResizer from "./components/IframeResizer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black text-white">
+        <IframeResizer />
         <main className="flex-1 flex flex-col">{children}</main>
         <footer className="border-t border-white/10 px-4 py-8 text-gray-500 text-xs">
           <div className="max-w-4xl mx-auto space-y-4">
