88bb29cc9499fd3e6732b4534b34a11efb3ba4c8
Luca Corsini <lcorsini@ea.com>
2026-05-11 10:45:48 +0100

    Revert "iframeResizer to get page height"
    This reverts commit 497abfe470df36a592072edfe139522273b10aeb.

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import IframeResizer from "./components/IframeResizer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +41,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black text-white">
-        <IframeResizer />
         <main className="flex-1 flex flex-col">{children}</main>
         <footer className="border-t border-white/10 px-4 py-8 text-gray-500 text-xs">
           <div className="max-w-4xl mx-auto space-y-4">
